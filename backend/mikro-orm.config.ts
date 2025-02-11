fddbb2ce6d12f419d4d320834e4891e92a694beb
Diogogc1 <diogogomescastro@outlook.com>
2025-02-11 15:50:07 -0300

    chore: Config Migrations and create Entities

---
+++ backend/mikro-orm.config.ts
@@ -0,0 +1,26 @@
+import { PostgreSqlDriver } from '@mikro-orm/postgresql';
+import { Article } from 'src/entities/article.entity';
+import { ArticleTag } from 'src/entities/article-tag.entity';
+import { Tag } from 'src/entities/tag.entity';
+import { User } from 'src/entities/user.entity';
+import { MikroOrmModuleSyncOptions } from '@mikro-orm/nestjs';
+import dotenv from 'dotenv';
+import { Migrator } from '@mikro-orm/migrations';
+import { EntityGenerator } from '@mikro-orm/entity-generator';
+import { SeedManager } from '@mikro-orm/seeder';
+
+dotenv.config();
+
+const config: MikroOrmModuleSyncOptions = {
+  entities: [User, Tag, Article, ArticleTag],
+  dbName: process.env.DB_NAME,
+  extensions: [Migrator, EntityGenerator, SeedManager],
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  driver: PostgreSqlDriver,
+  debug: true,
+};
+
+export default config;
--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,25 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './controllers/app.controller';
 import { AppService } from './services/app.service';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
-import { PostgreSqlDriver } from '@mikro-orm/postgresql';
-import * as dotenv from 'dotenv';
-import { User } from './entities/User.entity';
-
-dotenv.config();
+import config from 'mikro-orm.config';
 
 @Module({
-  imports: [
-    MikroOrmModule.forRoot({
-      entities: [User],
-      dbName: process.env.DB_NAME,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
-      driver: PostgreSqlDriver,
-      debug: true,
-    }),
-  ],
+  imports: [MikroOrmModule.forRoot(config)],
   controllers: [AppController],
   providers: [AppService],
 })
