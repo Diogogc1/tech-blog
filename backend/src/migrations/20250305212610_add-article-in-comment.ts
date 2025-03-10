a57db28339f5a088f3a5ef0920b1df6f03f4cc2f
Diogo Gomes <diogogomescastro@outlook.com>
2025-03-09 22:18:46 -0300

    fix: Change user_id for article_id in add-article-in-comment migration

--- backend/src/migrations/20250305212610_add-article-in-comment.ts
+++ backend/src/migrations/20250305212610_add-article-in-comment.ts
@@ -2,12 +2,12 @@ import type { Knex } from 'knex'
 
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.alterTable('comment', (table) => {
-    table.integer('user_id').unsigned().references('id').inTable('user')
+    table.integer('article_id').unsigned().references('id').inTable('article')
   })
 }
 
 export async function down(knex: Knex): Promise<void> {
   await knex.schema.alterTable('comment', (table) => {
-    table.dropColumn('user_id')
+    table.dropColumn('article_id')
   })
 }
